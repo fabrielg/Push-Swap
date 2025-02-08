dac535321a32489f932b324229c23b8c05cc9c34
fabrielg <gfrancois2003@gmail.com>
2025-02-08 20:51:37 +0100

    feat: update index when push, swap and rotate

--- stack_display.c
+++ stack_display.c
@@ -6,7 +6,7 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 00:10:31 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/02/07 14:51:50 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/02/08 20:31:18 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	stack_display(t_stack *stk)
 {
 	while (stk)
 	{
-		ft_printf("- %03d -\n", stk->value);
+		ft_printf("id: %d	- %03d -\n", stk->index, stk->value);
 		stk = stk->next;
 	}
 }
